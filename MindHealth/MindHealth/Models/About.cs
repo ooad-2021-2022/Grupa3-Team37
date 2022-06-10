8cf237cbc21ddebf786f7b03e8cf2e3b3c5987a6
igabela1 <igabela1@etf.unsa.ba>
2022-06-10 10:28:28 +0200

    Novi

---
+++ MindHealth/MindHealth/Models/About.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MindHealth.Models
+{
+    public class About : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
